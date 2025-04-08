31673639d3dc3b92133945c22e4af300df18b10f
fuelatank <fuelatank@gmail.com>
2025-04-08 21:19:31 +0800

    Fix retrieving images, audio, and video from content and add ContentDto::fromEntity()

--- src/main/java/backend/repository/ContentRepository.java
+++ src/main/java/backend/repository/ContentRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import backend.entity.ContentEntity;
@@ -12,6 +13,18 @@ import org.springframework.data.repository.query.Param;
 
 public interface ContentRepository extends JpaRepository<ContentEntity, Long> {
     List<ContentEntity> findByTags_Id(Long id);
+
+    @EntityGraph(attributePaths = {
+            "images",
+            "images.file",
+            "tags",
+            "audio",
+            "audio.file",
+            "video",
+            "video.file",
+            "createdBy" })
+    @Query("SELECT c FROM ContentEntity c")
+    Page<ContentEntity> findAllWithAssociations(Pageable pageable);
 
     @Query("SELECT c FROM ContentEntity c JOIN c.createdBy u WHERE u.username = :username")
     Page<ContentEntity> findByCreatedByUsername(@Param("username") String username, Pageable pageable);
--- src/main/java/backend/service/impl/ContentServiceImpl.java
+++ src/main/java/backend/service/impl/ContentServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import backend.entity.ContentEntity;
 import backend.entity.TagEntity;
@@ -26,8 +27,9 @@ public class ContentServiceImpl implements ContentService {
     private TagRepository tagRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public Page<ContentEntity> getContentList(Pageable pageable) {
-        return contentRepository.findAll(pageable);
+        return contentRepository.findAllWithAssociations(pageable);
     }
 
     @Override
@@ -77,6 +79,7 @@ public class ContentServiceImpl implements ContentService {
         content.setTags(tags);
         contentRepository.save(content);
     }
+
     @Override
     public Page<ContentEntity> search(String query, Pageable pageable) {
         return contentRepository.search(query, pageable);
